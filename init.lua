744105eed9de3652181ee633ce1f217ad60ff06f
Víctor Fernández Martínez <vfernandezmartinez@gmail.com>
2021-11-01 18:17:31 +0100

    Add light bricks

--- init.lua
+++ init.lua
@@ -19,10 +19,12 @@ minetest.register_node("house_walls:internal_wall", {
         sounds = default.node_sound_stone_defaults(),
 })
 
+
+
 minetest.register_node("house_walls:brick_wall", {
         description = S("Brick Wall"),
         paramtype2 = "facedir",
-        tiles = {"default_cloud.png","default_cloud.png","default_cloud.png","visible_brick.png","default_cloud.png","visible_brick.png"},
+        tiles = {"default_cloud.png","default_cloud.png","default_cloud.png","orange_brick.png","default_cloud.png","orange_brick.png"},
         is_ground_content = false,
         groups = {cracky = 2},
         sounds = default.node_sound_stone_defaults(),
@@ -32,7 +34,7 @@ minetest.register_node("house_walls:outside_brick_wall", {
         description = S("Outside Brick Wall"),
         paramtype2 = "facedir",
         place_param2 = 0,
-        tiles = {"visible_brick.png"},
+        tiles = {"orange_brick.png"},
         is_ground_content = false,
         groups = {cracky = 2},
         sounds = default.node_sound_stone_defaults(),
@@ -41,11 +43,43 @@ minetest.register_node("house_walls:outside_brick_wall", {
 minetest.register_node("house_walls:roof_brick_wall", {
         description = S("Roof Brick Wall"),
         paramtype2 = "facedir",
-        tiles = {"visible_brick.png","default_cloud.png","visible_brick.png","visible_brick.png","visible_brick.png","visible_brick.png"},
+        tiles = {"orange_brick.png","default_cloud.png","orange_brick.png","orange_brick.png","orange_brick.png","orange_brick.png"},
         is_ground_content = false,
         groups = {cracky = 2},
         sounds = default.node_sound_stone_defaults(),
 })
+
+
+
+minetest.register_node("house_walls:light_brick_wall", {
+        description = S("Light Brick Wall"),
+        paramtype2 = "facedir",
+        tiles = {"default_cloud.png","default_cloud.png","default_cloud.png","light_brick.png","default_cloud.png","light_brick.png"},
+        is_ground_content = false,
+        groups = {cracky = 2},
+        sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("house_walls:outside_light_brick_wall", {
+        description = S("Outside Light Brick Wall"),
+        paramtype2 = "facedir",
+        place_param2 = 0,
+        tiles = {"light_brick.png"},
+        is_ground_content = false,
+        groups = {cracky = 2},
+        sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("house_walls:roof_light_brick_wall", {
+        description = S("Roof Light Brick Wall"),
+        paramtype2 = "facedir",
+        tiles = {"light_brick.png","default_cloud.png","light_brick.png","light_brick.png","light_brick.png","light_brick.png"},
+        is_ground_content = false,
+        groups = {cracky = 2},
+        sounds = default.node_sound_stone_defaults(),
+})
+
+
 
 minetest.register_node("house_walls:dark_brick_wall", {
         description = S("Dark Brick Wall"),
