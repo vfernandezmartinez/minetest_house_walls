5ca0de7b71424980e7f56b444ffa6b170e22186b
Víctor Fernández Martínez <vfernandezmartinez@gmail.com>
2021-11-03 17:26:52 +0100

    Add tile walls

--- init.lua
+++ init.lua
@@ -1,10 +1,52 @@
-local S = default.get_translator
+
+local S = minetest.get_translator("house_walls")
+
+-- Floors
+minetest.register_node("house_walls:gray_tile_floor", {
+        description = S("Grey Tile Floor"),
+        paramtype2 = "facedir",
+        place_param2 = 0,
+        tiles = {"grey_tile.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png"},
+        is_ground_content = false,
+        groups = {cracky = 2},
+        sounds = default.node_sound_stone_defaults(),
+})
 
 minetest.register_node("house_walls:wooden_floor", {
         description = S("Wooden Floor"),
         paramtype2 = "facedir",
         place_param2 = 0,
         tiles = {"default_wood.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png"},
+        is_ground_content = false,
+        groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3, wood = 1},
+        sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("house_walls:acacia_wooden_floor", {
+        description = S("Acacia Wooden Floor"),
+        paramtype2 = "facedir",
+        place_param2 = 0,
+        tiles = {"default_acacia_wood.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png"},
+        is_ground_content = false,
+        groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3, wood = 1},
+        sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("house_walls:aspen_wooden_floor", {
+        description = S("Aspen Wooden Floor"),
+        paramtype2 = "facedir",
+        place_param2 = 0,
+        tiles = {"default_aspen_wood.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png"},
+        is_ground_content = false,
+        groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3, wood = 1},
+        sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("house_walls:junglewood_wooden_floor", {
+        description = S("Junglewood Floor"),
+        paramtype2 = "facedir",
+        place_param2 = 0,
+        tiles = {"default_junglewood.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png"},
         is_ground_content = false,
         groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3, wood = 1},
         sounds = default.node_sound_wood_defaults(),
@@ -20,8 +62,10 @@ minetest.register_node("house_walls:pine_floor", {
         sounds = default.node_sound_wood_defaults(),
 })
 
-minetest.register_node("house_walls:internal_wall", {
-        description = S("Internal Wall"),
+
+-- Inner walls
+minetest.register_node("house_walls:inner_wall", {
+        description = S("Inner Wall"),
         paramtype2 = "facedir",
         tiles = {"default_cloud.png"},
         is_ground_content = false,
@@ -29,8 +73,17 @@ minetest.register_node("house_walls:internal_wall", {
         sounds = default.node_sound_stone_defaults(),
 })
 
+minetest.register_node("house_walls:white_tile_inner_wall", {
+        description = S("White Tile Inner Wall"),
+        paramtype2 = "facedir",
+        tiles = {"default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","default_cloud.png","white_tile.png"},
+        is_ground_content = false,
+        groups = {cracky = 2},
+        sounds = default.node_sound_stone_defaults(),
+})
 
 
+-- Outer walls
 minetest.register_node("house_walls:brick_wall", {
         description = S("Brick Wall"),
         paramtype2 = "facedir",
@@ -41,7 +94,7 @@ minetest.register_node("house_walls:brick_wall", {
 })
 
 minetest.register_node("house_walls:outside_brick_wall", {
-        description = S("Outside Brick Wall"),
+        description = S("Orange Brick"),
         paramtype2 = "facedir",
         place_param2 = 0,
         tiles = {"orange_brick.png"},
@@ -71,7 +124,7 @@ minetest.register_node("house_walls:light_brick_wall", {
 })
 
 minetest.register_node("house_walls:outside_light_brick_wall", {
-        description = S("Outside Light Brick Wall"),
+        description = S("Light Brick"),
         paramtype2 = "facedir",
         place_param2 = 0,
         tiles = {"light_brick.png"},
